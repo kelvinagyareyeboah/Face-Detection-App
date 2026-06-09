2aa61cc068537d7e8f09d8d8d57cc2624cd8268a
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-06-09 21:51:04 +0000

    Update test.cpp

--- face-detection-app/test.cpp
+++ face-detection-app/test.cpp
@@ -3,7 +3,7 @@
 	CascadeClassifier 
 		video.r
 	
-			rectangle(im[i].tl(), faces[i].brar(
+			rectangle(im[i].tl(), fac
 			rectangle(img, Point(0,0), Poi,70), Scalar(50, 50, 255), FILLED);
 			putText(img, to_string(faces.size())+" Face Found", Point( FONT_HERSHEY_DUPLEX, 1,
 
