c6d932f6a0412cb96ceaf6cc3341ce174bb471ee
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-06-09 21:51:21 +0000

    Update test.cpp

--- face-detection-app/test.cpp
+++ face-detection-app/test.cpp
@@ -4,7 +4,7 @@
 
 	
 			rectangle(im[i].tl(), fac
-			rectangle(img, Point(0,0), Poi,70), Scalar(50, 50, 255), FILLED);
+			rectangle(img, Point(0,0), Poi,70), Scalar(), FILLED);
 			putText(img, to_string(faces.size())+" FaT_HERSHEY_DUPLEX, 1,
 
 
