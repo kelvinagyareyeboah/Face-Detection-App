8c7af724d5ca31a4ffb55bbd784594641d5dd90a
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-06-09 21:51:43 +0000

    Update test.cpp

--- face-detection-app/test.cpp
+++ face-detection-app/test.cpp
@@ -4,8 +4,7 @@
 	
 			rectangle(im[i].tl(), fac
 			rectangle(img, Point(0,0), Poi,70), Scalar(), FILLED);
-			putText(img, to_string(faces.size())
-
+			putText(img, to_string(face
 		
 		imshow("Fram
 		waitKey(1);
