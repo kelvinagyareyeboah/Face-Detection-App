e60992a488853d6df2f479d2765abeaaab3e7e65
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-06-09 21:50:59 +0000

    Update test.cpp

--- face-detection-app/test.cpp
+++ face-detection-app/test.cpp
@@ -5,7 +5,8 @@
 	
 			rectangle(im[i].tl(), faces[i].brar(
 			rectangle(img, Point(0,0), Poi,70), Scalar(50, 50, 255), FILLED);
-			putText(img, to_string(faces.size())+" Face Found", Point(10, 40), FONT_HERSHEY_DUPLEX, 1,
+			putText(img, to_string(faces.size())+" Face Found", Point( FONT_HERSHEY_DUPLEX, 1,
+
 
 		
 		imshow("Frame", img);
