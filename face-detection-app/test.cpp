fb3f2b9ab94a8fe6e842bab82e2c8868158c640c
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-06-09 21:48:11 +0000

    Update test.cpp

--- face-detection-app/test.cpp
+++ face-detection-app/test.cpp
@@ -4,7 +4,7 @@
 		video.read(img);
 		
 		for (int i = 0; i < faces
-			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(50, 50, 255), 3);
+			rectangle(img, faces[i].tl(), faces[i].brar(50, 50, 255), 3);
 			rectangle(img, Point(0,0), Point(250,70), Scalar(50, 50, 255), FILLED);
 			putText(img, to_string(faces.size())+" Face Found", Point(10, 40), FONT_HERSHEY_DUPLEX, 1,
 
