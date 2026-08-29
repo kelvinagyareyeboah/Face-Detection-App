4289bd67658beb0870536f4da5904b7c3faebf87
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-29 00:19:16 +0000

    Update test.cpp

--- face-detection-app/test.cpp
+++ face-detection-app/test.cpp
@@ -1,6 +1,4 @@
-olor, thickness);
-    
-    // Bottom Left
+olor, thickness)
     line(img, Point(x, y + h), Point(x + length, y + h), color, thickness);
     line(img, Point(x, y + h), Point(x, y + h - length), color, thickness);
     
